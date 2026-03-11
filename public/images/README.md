9c3e3330ad8dbd7a6c9b3bd2275ce82ca38cc4ba
April <44248273+AprilJC@users.noreply.github.com>
2026-03-11 11:01:14 +0800

    Add files via upload

---
+++ public/images/README.md
@@ -0,0 +1,5 @@
+# Post images
+
+Drop images here and reference them in Markdown as:
+
+![描述文字](/images/filename.jpg)
---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+export const SITE_TITLE = "My Blog";
+export const SITE_DESCRIPTION = "写字·记录·思考";
+export const SOCIAL_LINKS = {
+  github: "https://github.com/yourusername",
+  twitter: "https://twitter.com/yourusername",
+};
